e75989a89ef9271a60f39807142c53a44867a76d
Felipe Cardoso <felipedclc@gmail.com>
2020-07-09 17:58:12 -0300

    Special move - Castling

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -99,6 +99,25 @@ public class ChessMatch { // CLASSE ONDE CONTEM AS REGRAS DO JOGO
 			capturedPieces.add(capturedPiece);
 		}
 
+		// ROQUE PEQUENO
+		if (p instanceof King && target.getColumn() == source.getColumn() + 2) { // SE O REI SAIR DA POSIÇÃO INICIAL E ANDAR 2 CASAS
+			Position sourceT = new Position(source.getRow(), source.getColumn() + 3); // POSIÇÃO INICIAL TORRE
+			Position targetT = new Position(target.getRow(), target.getColumn() + 1); // MESMA POSIÇÃO DO REI, MAIS 1
+																						// COLUNA PARA DIREITA
+			ChessPiece rook = (ChessPiece) board.removePiece(sourceT); // RETIRANDO A POSIÇÃO INICIAL DA TORRE
+			board.placePiece(rook, targetT); // MUDANDO A PEÇA DE POSIÇÃO
+			rook.increaseMoveCount(); // INCREMENTO DA CONTAGEM DE MOVIMENTOS DA TORRE
+		}
+
+		// ROQUE GRANDE
+		if (p instanceof King && target.getColumn() == source.getColumn() - 2) {
+			Position sourceT = new Position(source.getRow(), source.getColumn() - 4);
+			Position targetT = new Position(target.getRow(), target.getColumn() - 1);
+			ChessPiece rook = (ChessPiece) board.removePiece(sourceT);
+			board.placePiece(rook, targetT);
+			rook.increaseMoveCount();
+		}
+
 		return capturedPiece;
 	}
 
@@ -111,6 +130,24 @@ public class ChessMatch { // CLASSE ONDE CONTEM AS REGRAS DO JOGO
 			board.placePiece(capturedPiece, target); // VOLTANDO A PEÇA CAPTURADA PARA O JOGO
 			capturedPieces.remove(capturedPiece); // REMOVENDO A PEÇA DA LISTA DE CAPTURADAS
 			piecesOnTheBoard.add(capturedPiece); // VOLTANDO A PEÇA CAPTURADA PARA A LISTA DO TABULEIRO
+		}
+
+		// ROQUE PEQUENO
+		if (p instanceof King && target.getColumn() == source.getColumn() + 2) { // SE O REI SAIR DA POSIÇÃO INICIAL E ANDAR 2 CASAS
+			Position sourceT = new Position(source.getRow(), source.getColumn() + 3); // POSIÇÃO INICIAL TORRE
+			Position targetT = new Position(target.getRow(), target.getColumn() + 1); // MESMA POSIÇÃO DO REI, MAIS COLUNA PARA DIREITA
+			ChessPiece rook = (ChessPiece) board.removePiece(targetT); // RETIRANDO A POSIÇÃO INICIAL DA TORRE
+			board.placePiece(rook, sourceT); // MUDANDO A PEÇA DE POSIÇÃO
+			rook.decreaseMoveCount(); // INCREMENTO DA CONTAGEM DE MOVIMENTOS DA TORRE
+		}
+
+		// ROQUE GRANDE
+		if (p instanceof King && target.getColumn() == source.getColumn() - 2) {
+			Position sourceT = new Position(source.getRow(), source.getColumn() - 4);
+			Position targetT = new Position(target.getRow(), target.getColumn() - 1);
+			ChessPiece rook = (ChessPiece) board.removePiece(targetT);
+			board.placePiece(rook, sourceT);
+			rook.decreaseMoveCount();
 		}
 	}
 
@@ -209,7 +246,7 @@ public class ChessMatch { // CLASSE ONDE CONTEM AS REGRAS DO JOGO
 		placeNewPiece('b', 1, new Knight(board, Color.WHITE));
 		placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
 		placeNewPiece('d', 1, new Queen(board, Color.WHITE));
-		placeNewPiece('e', 1, new King(board, Color.WHITE));
+		placeNewPiece('e', 1, new King(board, Color.WHITE, this));
 		placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
 		placeNewPiece('g', 1, new Knight(board, Color.WHITE));
 		placeNewPiece('h', 1, new Rook(board, Color.WHITE));
@@ -221,12 +258,12 @@ public class ChessMatch { // CLASSE ONDE CONTEM AS REGRAS DO JOGO
 		placeNewPiece('f', 2, new Pawn(board, Color.WHITE));
 		placeNewPiece('g', 2, new Pawn(board, Color.WHITE));
 		placeNewPiece('h', 2, new Pawn(board, Color.WHITE));
-		
+
 		placeNewPiece('a', 8, new Rook(board, Color.BLACK));
 		placeNewPiece('b', 8, new Knight(board, Color.BLACK));
 		placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
 		placeNewPiece('d', 8, new Queen(board, Color.BLACK));
-		placeNewPiece('e', 8, new King(board, Color.BLACK));
+		placeNewPiece('e', 8, new King(board, Color.BLACK, this));
 		placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
 		placeNewPiece('g', 8, new Knight(board, Color.BLACK));
 		placeNewPiece('h', 8, new Rook(board, Color.BLACK));
--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -2,13 +2,17 @@ package chess.pieces;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.Color;
 
 public class King extends ChessPiece {
 
-	public King(Board board, Color color) {
+	private ChessMatch chessMatch;
+
+	public King(Board board, Color color, ChessMatch chessMatch) {
 		super(board, color);
+		this.chessMatch = chessMatch;
 	}
 
 	@Override
@@ -19,6 +23,11 @@ public class King extends ChessPiece {
 	private boolean canMove(Position position) {
 		ChessPiece p = (ChessPiece) getBoard().piece(position); // PEGANDO A PEÇA (P) QUE ESTIVER NA POSIÇÃO
 		return p == null || p.getColor() != getColor(); // PEÇA P MOVE SE NA FRENTE FOR NUUL OU FOR UMA PEÇA ADVERSARIA
+	}
+
+	private boolean testRookCastling(Position position) { // METODO QUE TESTA SE A TORRE ESTÁ APTA PARA O "ROQUE"
+		ChessPiece p = (ChessPiece) getBoard().piece(position); // PEGANDO A POSIÇÃO DA PEÇA
+		return p != null && p instanceof Rook && p.getColor() == getColor() && p.getMoveCount() == 0; // CONDIÇÃO
 	}
 
 	@Override
@@ -56,23 +65,50 @@ public class King extends ChessPiece {
 		if (getBoard().positionExists(p) && canMove(p)) { // CONDIÇOES
 			mat[p.getRow()][p.getColumn()] = true; // PODE MOVER
 		}
-		
+
 		// ne
-		p.setValues(position.getRow() -1, position.getColumn() + 1);
+		p.setValues(position.getRow() - 1, position.getColumn() + 1);
 		if (getBoard().positionExists(p) && canMove(p)) { // CONDIÇOES
 			mat[p.getRow()][p.getColumn()] = true; // PODE MOVER
 		}
-		
+
 		// sw
-		p.setValues(position.getRow() +1, position.getColumn() - 1);
+		p.setValues(position.getRow() + 1, position.getColumn() - 1);
 		if (getBoard().positionExists(p) && canMove(p)) { // CONDIÇOES
 			mat[p.getRow()][p.getColumn()] = true; // PODE MOVER
 		}
-		
+
 		// se
-		p.setValues(position.getRow() +1, position.getColumn() + 1);
+		p.setValues(position.getRow() + 1, position.getColumn() + 1);
 		if (getBoard().positionExists(p) && canMove(p)) { // CONDIÇOES
 			mat[p.getRow()][p.getColumn()] = true; // PODE MOVER
+		}
+
+		// specialmove CASTLING
+		if (getMoveCount() == 0 && !chessMatch.getCheck()) {
+			// ROQUE PEQUENO
+			Position posT1 = new Position(position.getRow(), position.getColumn() + 3); // POSIÇÃO ONDE DEVE ESTAR A
+																						// TORRE NO LE
+			if (testRookCastling(posT1)) {
+				Position p1 = new Position(position.getRow(), position.getColumn() + 1); // POSIÇÃO DA CASA AO LADO DO
+																							// REI
+				Position p2 = new Position(position.getRow(), position.getColumn() + 2);
+				if (getBoard().piece(p1) == null && getBoard().piece(p2) == null) { // SE AS POSIÇÕES AO LADO ESTIVEREM
+																					// VAZIAS
+					mat[position.getRow()][position.getColumn() + 2] = true; // A PEÇA ANDA 2 CASA PARA O LADO
+				}
+			}
+
+			// ROQUE GRANDE
+			Position posT2 = new Position(position.getRow(), position.getColumn() + 4); 
+			if (testRookCastling(posT2)) {
+				Position p1 = new Position(position.getRow(), position.getColumn() - 1); 
+				Position p2 = new Position(position.getRow(), position.getColumn() - 2);
+				Position p3 = new Position(position.getRow(), position.getColumn() - 3);
+				if (getBoard().piece(p1) == null && getBoard().piece(p2) == null && getBoard().piece(p3) == null) { 
+					mat[position.getRow()][position.getColumn() - 2] = true; // A PEÇA ANDA 2 CASA PARA O LADO
+				}
+			}
 		}
 
 		return mat;
