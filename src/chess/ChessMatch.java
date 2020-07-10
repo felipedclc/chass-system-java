f5657ab94cd7b2b9875ec5565cb38da7632b66f0
Felipe Cardoso <felipedclc@gmail.com>
2020-07-10 13:59:05 -0300

    Special move - Promotion

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,5 +1,6 @@
 package chess;
 
+import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -22,6 +23,7 @@ public class ChessMatch { // CLASSE ONDE CONTEM AS REGRAS DO JOGO
 	private boolean check; // boolean JA COMEÇA VALENDO FALSE
 	private boolean checkMate;
 	private ChessPiece enPassantVulnerable;
+	private ChessPiece promoted;
 
 	private List<Piece> piecesOnTheBoard = new ArrayList<>();
 	private List<Piece> capturedPieces = new ArrayList<>();
@@ -52,6 +54,10 @@ public class ChessMatch { // CLASSE ONDE CONTEM AS REGRAS DO JOGO
 	public ChessPiece getEnPassantVulnerable() {
 		return enPassantVulnerable;
 	}
+	
+	public ChessPiece getPromoted() {
+		return promoted;
+	}
 
 	public ChessPiece[][] getPieces() { // RETORNA UMA MATRIZ DE PEÇAS DE XADREZ CORRESPONDENTE À "CHESS MATCH"
 		ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
@@ -81,10 +87,19 @@ public class ChessMatch { // CLASSE ONDE CONTEM AS REGRAS DO JOGO
 		if (testCheck(currentPlayer)) { // SE A PEÇA(REI) ESTIVER SE COLOCANDO EM CHEQUE
 			undoMove(source, target, capturedPiece); // DESFAZENDO O MOVIMENTO
 			throw new ChessException("You can't put yourself in check");
-		}
+		} 
 
 		ChessPiece movedPiece = (ChessPiece) board.piece(target);
 
+		// MOVIMENTO PROMOÇÃO (DO PEAO)
+		promoted = null;
+		if(movedPiece instanceof Pawn) {
+			if((movedPiece.getColor() == Color.WHITE && target.getRow() == 0) || (movedPiece.getColor() == Color.BLACK && target.getRow() == 7)) {
+				promoted = (ChessPiece)board.piece(target);
+				promoted = replacePromotedPiece("Q");
+			}
+		}
+		
 		check = (testCheck(opponent(currentPlayer))) ? true : false; // TESTANDO SE O OPONENTE ESTA EM CHEQUE
 
 		if (testCheckMate(opponent(currentPlayer))) { // TESTANDO SE HOUVE CHECK MATE
@@ -102,6 +117,32 @@ public class ChessMatch { // CLASSE ONDE CONTEM AS REGRAS DO JOGO
 		}
 
 		return (ChessPiece) capturedPiece;
+	}
+
+	public ChessPiece replacePromotedPiece(String type) {
+		if(promoted == null) {
+			throw new IllegalStateException("There is no piece to be promoted");
+		}
+		if(!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) { // EQUALS COMPARA SE UMA CLASSE(STRING) É IGUAL A OUTRA
+			throw new InvalidParameterException("Invalid type for promotion");
+		}
+		
+		Position pos = promoted.getChessPosition().toPosition(); // PEGANDO A POSIÇÃO DA PEÇA PROMOVIDA
+		Piece p = board.removePiece(pos); // REMOVENDO A PEÇA POS E GUARDANDO NA VARIAVEL P
+		piecesOnTheBoard.remove(p); // EXCLUINDO A PEÇA DA LISTA DE PEÇAS NO TABULEIRO
+		
+		ChessPiece newpPiece = newPiece(type, promoted.getColor());
+		board.placePiece(newpPiece, pos); // COLOCANDO A PEÇA NO LUGAR DA REMOVIDA(POS)
+		piecesOnTheBoard.add(newpPiece); // ADD NA LISTA
+		
+		return newpPiece;
+	}
+	
+	private ChessPiece newPiece(String type, Color color) { // METODO AUXILIAR
+		if(type.equals("B")) return new Bishop(board, color);
+		if(type.equals("N")) return new Knight(board, color);
+		if(type.equals("Q")) return new Queen(board, color);
+		return new Rook(board, color); 
 	}
 
 	private Piece makeMove(Position source, Position target) {
